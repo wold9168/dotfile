b9795637410210b809886db720b721f43f4bd022
wold9168 <moewold@outlook.com>
2026-08-17 13:22:41 +0800

    fix: update kitty, zed and oh-my-zsh submodules and reorder copy logic in dotfile-init.sh
    Signed-off-by: wold9168 <moewold@outlook.com>

--- dotfile-init.sh
+++ dotfile-init.sh
@@ -2,7 +2,6 @@
 
 USER=wold9168
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd);
-[[ "$SCRIPT_DIR" == "/home/${USER}" ]] || cp -a "$SCRIPT_DIR/." "/home/${USER}/"
 
 git submodule update --init --remote
 source .zshrc
@@ -10,3 +9,5 @@ source .zshrc
 # Zsh plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+[[ "$SCRIPT_DIR" == "/home/${USER}" ]] || cp -a "$SCRIPT_DIR/." "/home/${USER}/"

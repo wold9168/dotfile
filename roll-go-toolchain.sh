a2de195527e0dda6a538dfdc4590a492b5abb331
wold9168 <moewold@outlook.com>
2026-08-24 00:25:26 +0800

    Add roll-go-toolchain.sh script
    This script is designed for installing necessary pkgs when goenv use a new version of go-toolchain.
    
    Signed-off-by: wold9168 <moewold@outlook.com>

---
+++ roll-go-toolchain.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+packages=(
+    "golang.org/x/tools/gopls@latest"
+    "github.com/go-delve/delve/cmd/dlv@latest"
+    "-tags most github.com/xo/usql@latest"
+)
+
+BOLD_WHITE_ON_BLUE="\033[1;37;44m"
+RESET="\033[0m"
+
+for pkg in "${packages[@]}"; do
+    echo -e "${BOLD_WHITE_ON_BLUE}${pkg}${RESET}"
+    go install "${pkg}"
+done
